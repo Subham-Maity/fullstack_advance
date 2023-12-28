0826565137ddb866760666a62dd3ea8ab488e0f3
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-12-28 05:52:30 +0530

    feat: JWT_auth: Backend "strictQuery", true

--- server/config/dbConnect.ts
+++ server/config/dbConnect.ts
@@ -1,23 +1,24 @@
-import mongoose, {ConnectOptions, Mongoose} from "mongoose";
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
 import config from "./default";
 import log from "../src/utils/logger/logger";
 
 const connectDB = async (): Promise<void> => {
-    try {
-        const uri: string =
-            process.env.MONGO_URL || `mongodb://localhost:${config.port}/mydatabase`;
+  try {
+    const uri: string =
+      process.env.MONGO_URL || `mongodb://localhost:${config.port}/mydatabase`;
+    //It is recommended to set useNewUrlParser, useUnifiedTopology, and useCreateIndex to true.
+    mongoose.set("strictQuery", true);
+    const con = await mongoose.connect(uri);
 
-        const con = await mongoose.connect(uri);
-
-        log.info(`MongoDB Connected: ` + `${con.connection.host}`);
-    } catch (error) {
-        if (error instanceof Error) {
-            console.log(`Error: ${error.message}`);
-        } else {
-            console.log(`An unknown error occurred`);
-        }
-        process.exit(1);
+    log.info(`MongoDB Connected: ` + `${con.connection.host}`);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`Error: ${error.message}`);
+    } else {
+      console.log(`An unknown error occurred`);
     }
+    process.exit(1);
+  }
 };
 
 export default connectDB;
